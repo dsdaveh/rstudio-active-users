8876ced77e496cf469505c7b341415cbe611c1f8
James Blair <james.m.blair09@gmail.com>
2020-11-20 00:04:39 +0000

    Add additional debug messages; Write resulting data to .csv file

--- scripts/mau-rsp.R
+++ scripts/mau-rsp.R
@@ -6,6 +6,9 @@ log_path <- "/var/lib/rstudio-server/audit/r-sessions/r-sessions.csv"
 # Set minimum date - default is 1 year ago
 min_date <- as.POSIXct(Sys.Date() - 365)
 
+# Set CSV path for data write
+csv_path <- paste0("./rsp-user-counts-", Sys.time(), ".csv")
+
 # Set debug value
 debug <- FALSE
 
@@ -14,20 +17,30 @@ print_debug <- function(msg) {
   if(debug) print(msg)
 }
 
+print_dims <- function(dat) {
+  dims <- dim(dat)
+  print_debug(paste0("Data dimensions: ", paste0(dims[1], " x ", dims[2])))
+}
+
 # Parse arguments if run as CLI
 if (!interactive()) {
   library(argparser, quietly = TRUE)
   p <- arg_parser("Monthly Active RStudio User Counts")
   p <- add_argument(parser = p, 
                     arg = "--log-path", 
-                    help = paste0("Path to RStudio Session logs. Default: ", log_path),
+                    help = paste0("Path to RStudio Session logs"),
                     type = "character",
                     default = log_path)
   p <- add_argument(parser = p,
                     arg = "--min-date",
-                    help = paste0("Minimum date to compute monthly counts. Default: ", min_date),
+                    help = paste0("Minimum date to compute monthly counts"),
                     type = "character",
                     default = as.character(min_date))
+  p <- add_argument(parser = p,
+                    arg = "--csv-path",
+                    help = paste0("Path to write .csv file of user counts"),
+                    type = "character",
+                    default = csv_path)
   p <- add_argument(parser = p,
                     arg = "--debug",
                     help = "Enable debug output",
@@ -44,30 +57,42 @@ if (!interactive()) {
 # Read log data
 print_debug(paste0("Reading data: ", log_path))
 log_data <- read.csv(log_path, stringsAsFactors = FALSE)
-print_debug(paste0("Data dimensions: ", dim(log_data)))
+print_dims(log_data)
 
 # Convert timestamp from numeric
 print_debug("Converting timestamp")
 log_data$timestamp <- as.POSIXct(log_data$timestamp / 1000, origin = "1970-01-01")
-print_debug(paste0("Data dimensions: ", dim(log_data)))
+print_dims(log_data)
 
 # Filter to events >= min_date
-
+print_debug(paste0("Filtering to events >= ", min_date))
 log_data <- log_data[log_data$timestamp >= min_date,]
+print_dims(log_data)
 
 # Extract month and year
+print_debug("Extracting month from timestamp")
 log_data$month <- format(log_data$timestamp, format = "%m-%Y")
 
 # Filter only to "session_start" events
+print_debug("Filtering to session_start events")
 log_data <- log_data[grepl("session_start", log_data$type),]
+print_dims(log_data)
 
 # Summarize by unique username and month combinations
+print_debug("Summarizing by unique username and month combinations")
 log_data <- unique(log_data[,c("username", "month")])
+print_dims(log_data)
 
 # Calculate observations per month, which is equivalent to the number of active 
 # users per month
+print_debug("Calculating user counts by month")
 user_counts <- as.data.frame(table(log_data$month))
 names(user_counts) <- c("Month", "Active User Count")
+print_dims(user_counts)
+
+# Write CSV
+print_debug(paste0("Writing user counts data to ", csv_path))
+write.csv(user_counts, csv_path)
 
 # Print final user counts
 user_counts
