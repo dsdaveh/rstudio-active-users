596e131dcf96dd9f2a7a5ea0e71c1f05fc70a18e
James Blair <james.m.blair09@gmail.com>
2020-12-07 17:02:54 -0700

    Add mau-combine script

---
+++ scripts/mau-combine.R
@@ -0,0 +1,60 @@
+#! /usr/local/bin/Rscript
+
+# Set default paths
+rsp_mau_path <- NULL
+rsc_mau_path <- NULL
+
+# Output path
+csv_path <- paste0("./combined-mau-counts-", Sys.time(), ".csv")
+
+# Print Debug utility
+print_debug <- function(msg) {
+  if(debug) cat(msg, "\n")
+}
+
+# Set debug value
+debug <- FALSE
+
+# Parse arguments if run as CLI
+if (!interactive()) {
+  library(argparser, quietly = TRUE)
+  p <- arg_parser("Combine Monthly Active User Counts")
+  p <- add_argument(parser = p, 
+                    arg = "--rsp-path", 
+                    help = "Path to output from mau-rsp",
+                    type = "character",
+                    default = rsp_mau_path)
+  p <- add_argument(parser = p,
+                    arg = "--rsc-path",
+                    help = "Path to output from mau-rsc",
+                    type = "character",
+                    default = rsc_mau_path)
+  p <- add_argument(parser = p,
+                    arg = "--output",
+                    help = "Path to write combined .csv file of user counts",
+                    type = "character",
+                    default = csv_path)
+  p <- add_argument(parser = p,
+                    arg = "--debug",
+                    help = "Enable debug output",
+                    flag = TRUE)
+  
+  argv <- parse_args(p)
+  
+  rsp_mau_path <- argv$rsp_path
+  rsc_mau_path <- argv$rsc_path
+  csv_path <- argv$output
+}
+
+print_debug(paste0("Reading ", rsp_mau_path))
+rsp_counts <- read.csv(rsp_mau_path, stringsAsFactors = FALSE)
+
+print_debug(paste0("Reading ", rsc_mau_path))
+rsc_counts <- read.csv(rsc_mau_path, stringsAsFactors = FALSE)
+
+print_debug("Combining user counts")
+combined_counts <- rbind(rsp_counts, rsc_counts)
+combined_counts <- combined_counts[order(combined_counts$month, combined_counts$user),]
+
+print_debug(paste0("Writing combined counts to ", csv_path))
+write.csv(combined_counts, csv_path)
